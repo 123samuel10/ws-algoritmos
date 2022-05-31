f539b5a910ff10b2fabea5d761c5a9fce776fd59
Samuel <samuelsalcedoramirez@gmail.com>
2022-05-31 17:19:21 -0500

    ajuster

--- ProyectoSamuel/main.js
+++ ProyectoSamuel/main.js
@@ -1,25 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var localStorage = window.localStorage
-    var infoProductos = localStorage.getItem("infoProductos") ? localStorage.getItem("infoProductos").split(",") : []
+document.addEventListener("DOMContentLoaded", function () {//ponemos el evento DOMContentLoaded, en el cual consiste en ejeecutar todo el codigo de js,cuando todo el html ha cargado
+    var localStorage = window.localStorage//Guardamos el localStorage en una variable
+    var infoProductos=localStorage.getItem("infoProductos") ? localStorage.getItem("infoProductos").split(",") : [] //infoProductos es la variable donde estan almacenados
+    //los  productos. y esto es un If ternario.
     
     organizarData()
     localStorage.clear()// con esto limpiamos el localStorage
 
-    // mostrarProductos(infoProductos)
+    mostrarProductos(infoProductos)
+    
     
     var totalProductos = infoProductos.length
     document.getElementById("totalProductos").value = totalProductos
 
-    var valorTotalProductos = obtenerTotalPrecios()
-    document.getElementById("valorTotalProductos").value = valorTotalProductos
-
-
+    
+ 
     //con la funcion agregar al inicio, como lo dice su nombre guardamos en el arreglo lo que dijite el usuario y se agregara en la zona principal mediante el unshift(metodo el arreglo)
     agregarInicio = function agregarInicio() {
         infoProductos.unshift([document.getElementById("nombre").value,document.getElementById("categoria").value, document.getElementById("precio").value, document.getElementById("cantidad").value])
         localStorage.setItem('infoProductos', infoProductos)
         mostrarProductos(infoProductos)
+
     }
+
+
 
 
     //aqui agregamos lo que digite el usuario al final (mediante el metod push) y se guardara todo en un arreglo 
@@ -28,18 +31,27 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem('infoProductos', infoProductos)
         mostrarProductos(infoProductos)
     }
-
+  //con esta funcion mostramos en el cuadro hecho por html, la info que digite el usuario
+  function mostrarProductos(infoProductos) {
+    document.getElementById("listado").value = "";
+    infoProductos.forEach(producto => {
+        document.getElementById("listado").value += `Nombre: ${producto[0]}, Categoria: ${producto[1]}, Precio: ${producto[2]}, Cantidad: ${producto[3]} \n`;
+    });
+}
+    //Cuenta la cantidad de elementos o productos que tiene infoProductos de acuerdo a la categoria seleccionada
     contarCategoria = function contarCategoria() {
         let categoria = document.getElementById("categoriaProductosTotal").value
         let totalProductosCategoria = infoProductos.filter(producto => producto[1]===categoria)
         document.getElementById("totalProductosCategoria").value = totalProductosCategoria.length
     }
-
+   //con esta funcion buscamos el producto
     buscarProducto = function buscarProducto() {
         let nombreProducto = document.getElementById("nombreProducto").value
         let producto = infoProductos.filter(producto => producto[0] === nombreProducto)
         document.getElementById("productoBuscado").value = `Nombre: ${producto[0][0]}, Categoria: ${producto[0][1]}, Precio: ${producto[0][2]}, Cantidad: ${producto[0][3]}`
     }
+
+    //elimina un producto de acuerdo a el nombre que digite el usuario
 
     eliminarProducto = function eliminarProducto(){
         let nombreProductoEliminar = document.getElementById("nombreProductoEliminar").value
@@ -50,6 +62,7 @@ document.addEventListener("DOMContentLoaded", function () {
         mostrarProductos(infoProductos)
     }
 // --------------------------------------------------------------------------------------------------------------
+//Agrega un unidad al producto cuyo nombre ingrese el usuario
     agregarCantidad = function agregarCantidad(){
         let nombreProductoAgregar = document.getElementById("nombreProductoAgregar").value
         infoProductos.forEach(producto => { 
@@ -60,7 +73,7 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem('infoProductos', infoProductos)
         mostrarProductos(infoProductos)
     }
-
+//Disminuye  una unidad al producto cuyo nombre ingrese el usuario
     disminuirCantidad = function disminuirCantidad(){
         let nombreProductoDisminuir = document.getElementById("nombreProductoDisminuir").value
         infoProductos.forEach(producto => { 
@@ -71,6 +84,20 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem('infoProductos', infoProductos)
         mostrarProductos(infoProductos)
     }
+    //con esta funcion se obtiene el total de los productos
+    var valorTotalProductos = obtenerTotalPrecios()
+    document.getElementById("valorTotalProductos").value = valorTotalProductos
+
+
+    function obtenerTotalPrecios(){
+        let valorTotal = 0
+        infoProductos.forEach(producto => {
+            valorTotal+=Number(producto[2])
+        })
+        return valorTotal
+    }
+
+
 
     listarAlfabetico = function listarAlfabetico() {
         infoProductos.sort(function (a, b) {
@@ -99,19 +126,44 @@ document.addEventListener("DOMContentLoaded", function () {
         infoProductos = aux
     }
 
-    //con esta funcion mostramos en el cuadro hecho por html, la info que digite el usuario
-    function mostrarProductos(infoProductos) {
-        document.getElementById("listado").value = "";
-        infoProductos.forEach(producto => {
-            document.getElementById("listado").value += `Nombre: ${producto[0]}, Categoria: ${producto[1]}, Precio: ${producto[2]}, Cantidad: ${producto[3]} \n`;
-        });
-    }
 
-    function obtenerTotalPrecios(){
-        let valorTotal = 0
-        infoProductos.forEach(producto => {
-            valorTotal+=Number(producto[2])
-        })
-        return valorTotal
-    }
-});
+    
+ })
+
+
+ //utilizando el map (funcion)
+let mostrar=[];
+
+function multiplicadno(){
+    let precios=document.getElementById('precio').value;
+    precios=parseInt(precios)
+    mostrar.push(precios);
+    alert('el precio es:'+' '+ mostrar)
+   
+}
+
+function mostrar2(){
+    var resultado=mostrar.map(function(x){
+        return x * 2;
+    })
+    alert(resultado)
+}
+
+
+
+// ---------------------------------------------------------------------
+//utilizando el metodo reduce (funcion)
+let reducion=[];
+function reducir(){
+    let categoriasReducir=document.getElementById('cantidad').value;
+    categoriasReducir=parseInt(categoriasReducir);
+    reducion.push(categoriasReducir);
+    alert('por ahora van '+ ' '+ reducion+' '+'cantidades')
+    
+}
+function enviarreducion(){
+    var resultado2=reducion.reduce((acc,item)=>{
+        return acc=acc+item;
+    })
+    alert(`El total del numero cantidades es ${resultado2}`)
+}
